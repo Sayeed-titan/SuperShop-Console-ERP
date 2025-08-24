e59363e5b54693b5e8a2bc5b3e635a33b6895e43
Sayeed-titan <sayeed1286265@gmail.com>
2025-08-24 23:38:49 +0600

    Added CrudService and OrderService Interfaces

---
+++ SuperShop.Application/Interfaces/ICrudService.cs
@@ -0,0 +1,19 @@
+﻿using SuperShop.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperShop.Application.Interfaces
+{
+    public interface ICrudService<T> where T : class, IEntity
+    {
+        IEnumerable<T> GetAll();
+        T? GetById(int id);
+        T Add(T entity);
+        bool Update(T entity);
+        bool Delete(int id);
+        int Count();
+    }
+}
---
+++ SuperShop.Application/Interfaces/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using SuperShop.Domain.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperShop.Application.Interfaces
+{
+    public interface IOrderService
+    {
+        Order CreateOrder(int customerId, List<(int productId, int quantity)> items);
+        IEnumerable<Order> GetAll();
+        IEnumerable<Order> GetByCustomer(int customerId);
+        bool CancelOrder(int orderId);
+    }
+}
